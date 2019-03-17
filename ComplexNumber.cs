2fdee337a45a529ef3f651d4060a44e0d91e579f
Edson Bruno <brunoudia1@gmail.com>
2019-03-16 23:02:21 -0300

    Arquivos iniciais do projeto

---
+++ ComplexNumber.cs
@@ -0,0 +1,50 @@
+﻿namespace Aula3ex4
+{
+    class ComplexNumber
+    {
+        private double realPart;
+        private double imaginaryPart;
+        
+        public double RealPart {
+            get {
+                return realPart;
+            }
+            set {
+                realPart = value;
+            }
+        }
+        public double ImaginaryPart
+        {
+            get
+            {
+                return imaginaryPart;
+            }
+            set
+            {
+                imaginaryPart = value;
+            }
+        }
+
+        //Construtor da classe
+
+        public ComplexNumber(double realPart, double imaginaryPart) {
+            this.realPart = realPart;
+            this.imaginaryPart = imaginaryPart;
+        }
+
+
+        //Métodos retornam um novo objeto com o valor da operação
+        public ComplexNumber addition(ComplexNumber complexNumber) {
+             double resultRealPart = RealPart + complexNumber.RealPart;
+            double resultImaginaryPart = ImaginaryPart + complexNumber.ImaginaryPart;
+            return new ComplexNumber(resultRealPart, resultImaginaryPart);
+        }
+
+        public ComplexNumber subtraction(ComplexNumber complexNumber)
+        {
+            double resultRealPart = RealPart - complexNumber.RealPart;
+            double resultImaginaryPart = ImaginaryPart - complexNumber.ImaginaryPart;
+            return new ComplexNumber(resultRealPart, resultImaginaryPart);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula3ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criação de objetos para o calculo da função de números complexos
+            ComplexNumber N1 = new ComplexNumber(2.8, 4);
+            ComplexNumber N2 = new ComplexNumber(4, 7.7);
+
+            ComplexNumber N3 = N1.addition(N2);
+            ComplexNumber N4 = N1.addition(N2).subtraction(N3);
+
+            //Mostra o resultado final na tela
+            Console.WriteLine($"{ N4.RealPart.ToString()} + { N4.ImaginaryPart.ToString()} i");
+
+            //Para impedir que o programa finalize de forma imediata
+            Console.ReadKey();
+
+        }
+    }
+}
